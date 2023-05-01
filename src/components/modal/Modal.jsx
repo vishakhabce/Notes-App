1bad1735e6c6ac1af9a25b557f91551c98361f70
Vishakha Kumari <vishakhabce@gmail.com>
2023-05-01 19:15:34 +0530

    modal layout completed

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,30 +2,33 @@ import React, { useState } from 'react';
 import './module.Modal.css';
 
 const Modal = ({ isOpen, onClose }) => {
-  const [title, setTitle] = useState('');
-  const [color, setColor] = useState('');
+    const [title, setTitle] = useState('');
+    const [color, setColor] = useState('');
 
-  const handleCreate = () => {
-    // Do something with the title and color values
-    onClose();
-  };
+    const handleCreate = () => {
+        onClose();
+    };
 
-  return (
-    <div className={`modal ${isOpen ? 'open' : ''}`} onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <div>Create New Notes Group</div>
-        <label htmlFor="title">Group Name</label>
-        <input type="text" id="title" value={title} placeholder='Enter your group name....' onChange={(e) => setTitle(e.target.value)} />
-        <label htmlFor="color">Choose color</label>
-        <div className="color-chooser">
-          <span className="color-circle red" onClick={() => setColor('red')}></span>
-          <span className="color-circle green" onClick={() => setColor('green')}></span>
-          <span className="color-circle blue" onClick={() => setColor('blue')}></span>
+    return (
+        <div className={`modal ${isOpen ? 'open' : ''}`} onClick={onClose}>
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                <div className='modal-text'>Create New Notes Group</div>
+                <div className='title-wrapper'>
+                <label htmlFor="title" className='modal-text'>Group Name</label>
+                <input type="text" id="title" value={title} placeholder='Enter your group name....' onChange={(e) => setTitle(e.target.value)} />
+                </div>
+               <div className='color-wrapper'>
+               <label htmlFor="color" className='modal-text'>Choose color</label>
+                <div className="color-chooser">
+                    <span className="color-circle red" onClick={() => setColor('red')}></span>
+                    <span className="color-circle green" onClick={() => setColor('green')}></span>
+                    <span className="color-circle blue" onClick={() => setColor('blue')}></span>
+                </div>
+               </div>
+                <button  onClick={handleCreate}>Create</button>
+            </div>
         </div>
-        <button onClick={handleCreate}>Create</button>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Modal;
